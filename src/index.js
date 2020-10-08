a5119ec62ef579a4a48abd0d15ff6696e090cb9a
AB <austinbuhler@gmail.com>
2020-10-08 13:57:56 -0400

    GET and POST Complete

--- src/index.js
+++ src/index.js
@@ -12,4 +12,53 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
+    fetch("http://localhost:3000/toys")
+      .then(response => response.json())
+      .then(toys => {
+        for (const toy of toys) {
+          renderToy(toy)
+        }
+      })
+
+    function renderToy(toy){
+      const toyCollection = document.querySelector('#toy-collection')
+
+      const cardDiv = document.createElement('div')
+      cardDiv.classList.add('card')
+  
+      cardDiv.innerHTML = `
+        <h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p>${toy.likes} Likes </p>
+        <button class="like-btn">Like <3</button>
+      `
+      toyCollection.append(cardDiv)
+    }
+
+    const addToyForm = document.querySelector('.add-toy-form')
+
+    addToyForm.addEventListener("submit", function(e){
+      e.preventDefault()
+      let newToy = {
+        name: e.target.name.value,
+        image: e.target.image.value,
+        likes: 0
+      }
+
+      let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify(newToy)
+      }
+
+      fetch("http://localhost:3000/toys", configObj)
+        .then(response => response.json())
+        .then(newToy => renderToy(newToy))
+    })
+
 });
